fe8b5b7935f77265a074438eac2f1019d61f5018
Sh0ckWav329 <106288324+Sh0ckWav329@users.noreply.github.com>
2022-12-27 19:12:13 +0800

    Add Game Category

--- springsecurity/src/main/java/biz/global77/springsecurity/controller/GameController.java
+++ springsecurity/src/main/java/biz/global77/springsecurity/controller/GameController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import biz.global77.springsecurity.model.Game;
 import biz.global77.springsecurity.service.GameService;
 
 @Controller
@@ -19,4 +22,36 @@ public class GameController {
 		model.addAttribute("listOfGames", gameService.getAllGames());
 		return "forAuthenticated/listOfGames";
 	}
+	
+	//Adding New Game
+	@GetMapping("/showNewGameForm")
+	public String showNewGameForm(Model model) {
+		//Create Model Attribute to bind form data
+		Game game = new Game();
+		model.addAttribute("game", game);
+		return "forAuthenticated/new_game";
+	}
+	
+	@PostMapping("/saveGame")
+	public String saveGame(@ModelAttribute("game") Game game) {
+		// Save game to database
+		gameService.saveGame(game);
+		return "redirect:/";
+	}
+
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
