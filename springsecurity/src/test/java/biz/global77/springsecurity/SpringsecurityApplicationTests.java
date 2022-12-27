67932724a2846ec5641003bb34ad46ee98d4b49e
Sh0ckWav329 <106288324+Sh0ckWav329@users.noreply.github.com>
2022-12-27 11:15:00 +0800

    first commit

---
+++ springsecurity/src/test/java/biz/global77/springsecurity/SpringsecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package biz.global77.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
