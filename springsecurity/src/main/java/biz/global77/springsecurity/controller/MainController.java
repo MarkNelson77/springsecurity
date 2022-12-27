ec1e63d5eb03801de26334822f469ad33708c1bd
Sh0ckWav329 <106288324+Sh0ckWav329@users.noreply.github.com>
2022-12-27 13:49:52 +0800

    Adding controllers

--- springsecurity/src/main/java/biz/global77/springsecurity/controller/MainController.java
+++ springsecurity/src/main/java/biz/global77/springsecurity/controller/MainController.java
@@ -1,5 +1,21 @@
 package biz.global77.springsecurity.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MainController {
+	
+	@GetMapping("/home")
+	public String home() {
+		return "home";
+	}
+	
+	@GetMapping("/access-denied")
+	public String accessDenied() {
+		return "access-denied";
+	}
 
 }
+
+
--- springsecurity/src/main/java/biz/global77/springsecurity/controller/PageController.java
+++ springsecurity/src/main/java/biz/global77/springsecurity/controller/PageController.java
@@ -1,5 +1,31 @@
 package biz.global77.springsecurity.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/page")
 public class PageController {
 
+    @GetMapping("/all")
+    public String forAll() {
+        return "forAll/page1";
+    }
+
+    @GetMapping("/manager")
+    public String forManager() {
+        return "forAuthenticated/page2";
+    }
+
+    @GetMapping("/admin")
+    public String forAdmin() {
+        return "forAuthenticated/page3";
+    }
+
+    @GetMapping("/official")
+    public String forOfficial() {
+        return "forAuthenticated/page4";
+    }
+    
 }
